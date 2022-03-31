2b3e7e8020601e8832489071dd269667221d8106
Petteri <26197131+PetteriM1@users.noreply.github.com>
2022-03-31 13:55:40 +0300

    Disabled worlds

--- src/main/java/me.petterim1.silkspawners/Main.java
+++ src/main/java/me.petterim1.silkspawners/Main.java
@@ -12,17 +12,26 @@ import cn.nukkit.item.enchantment.Enchantment;
 import cn.nukkit.plugin.PluginBase;
 import nukkitcoders.mobplugin.entities.block.BlockEntitySpawner;
 
+import java.util.List;
+
 public class Main extends PluginBase implements Listener {
 
     private boolean useMobPlugin;
     private boolean dropMob;
+    private List<String> worldsDisabled;
 
     public void onEnable() {
         saveDefaultConfig();
+        if (getConfig().getInt("configVersion") != 2) {
+            getLogger().warning("Outdated config file");
+        }
         dropMob = getConfig().getBoolean("dropMob");
+        worldsDisabled = getConfig().getStringList("worldsDisabled");
         if (getServer().getPluginManager().getPlugin("MobPlugin") != null) {
             useMobPlugin = true;
-            getLogger().debug("MobPlugin found!");
+            getLogger().debug("MobPlugin found");
+        } else {
+            getLogger().debug("MobPlugin not found");
         }
         getServer().getPluginManager().registerEvents(this, this);
     }
@@ -31,7 +40,7 @@ public class Main extends PluginBase implements Listener {
     public void onBreak(BlockBreakEvent e) {
         if (e.getBlock().getId() == BlockID.MONSTER_SPAWNER) {
             Item item = e.getItem();
-            if (item != null && item.isPickaxe() && e.getPlayer().hasPermission("silkspawners.use") && item.getEnchantment(Enchantment.ID_SILK_TOUCH) != null) {
+            if (item != null && item.isPickaxe() && e.getPlayer().hasPermission("silkspawners.use") && !worldsDisabled.contains(e.getPlayer().getLevel().getName()) && item.getEnchantment(Enchantment.ID_SILK_TOUCH) != null) {
                 if (useMobPlugin && dropMob) {
                     BlockEntity be = e.getBlock().getLevel().getBlockEntity(e.getBlock());
                     if (be instanceof BlockEntitySpawner) {
